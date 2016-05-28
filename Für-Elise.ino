fc8a76758d786a929636aba2a92fa53eabd08ba2
Abhishek Ghosh <dr.abhishek_ghosh@hotmail.com>
2016-05-28 22:19:19 +0530

    Create Für-Elise.ino

---
+++ Für-Elise.ino
@@ -0,0 +1,275 @@
+// * Caterina's Fur Elise on Arduino
+// * Original Composition : Für Elise
+// * Composed by : Ludwig van Beethoven
+// * Coded By GitHub User @spara - https://gist.github.com/spara/1832855
+// * Use BSD Clause 2 License for Distribution
+// * Collection by GitHub User @abhishekghosh - https://github.com/AbhishekGhosh/Arduino-Buzzer-Tone-Codes
+
+void setup() {
+  pinMode(10, OUTPUT);
+}
+void loop() {
+  
+  // play e4
+  delay(600);
+  tone(10, 329.63, 300);
+  delay(350);
+  // play d4# 
+  tone(10, 311.13, 300);
+  delay(350);
+  // play e4
+  tone( 10, 329.63, 300);
+  delay(350);
+  // play d4# 
+  tone( 10,311.13, 300);
+  delay(350);
+  // play e4
+  tone(10, 329.63, 300);
+  delay(350);
+  // play b3
+  tone( 10, 246.94, 300);
+  delay(400);
+  // play d4
+  tone(10, 293.66,300);
+  delay(400);
+  // play c4
+  tone(10, 261.63,300);
+  delay(400);
+  // play a3
+  tone(10, 220, 900);
+  delay(1000);
+  // play d3
+  tone(10,146.83, 300);
+  delay(350);
+  //play f3
+  tone(10, 174.61, 300);
+  delay(400);
+  //play a3
+  tone(10, 220, 300);
+  delay(400);
+  // play b3
+  tone(10, 246.94, 900);
+  delay(1000);
+  // play f3
+  tone(10, 174.61, 300);
+  delay(400);
+  // play a3#
+  tone(10, 233.08, 300);
+  delay(400);
+  // play b3
+  tone(10, 246.94, 300);
+  delay(400);
+  // play c4
+  tone(10, 261.63, 900);
+  delay(1000);
+  delay(300);
+  // play e4
+  tone(10, 329.63, 300);
+  delay(400);
+  // play d4#
+  tone(10, 311.13, 300);
+  delay(400);
+  // play e4
+  tone(10, 329.63, 300);
+  delay(400);
+  // play d4#
+  tone(10, 311.13, 300);
+  delay(400);
+  // play e4
+  tone(10, 329.63, 300);
+  delay(400);
+  // play b3
+  tone(10, 246.94, 300);
+  delay(400);
+  // play d4
+  tone(10, 293.66, 300);
+  delay(400);
+  // play c4
+  tone(10, 261.63, 300);
+  delay(400);
+  // play a3
+  tone(10, 220, 900);
+  delay(1000);
+  // play d3
+  tone(10, 146.83, 300);
+  delay(400);
+  // play f3
+  tone(10, 174.61, 300);
+  delay(400);
+  // play a3
+  tone(10, 220, 300);
+  delay(400);
+  // play b3
+  tone(10, 246.94, 900);
+  delay(1000);
+  // play f3
+  tone(10, 174.61, 300);
+  delay(400);
+  // play c4
+  tone(10, 261.63, 300);
+  delay(400);
+  // play b3
+  tone(10, 246.94, 300);
+  delay(400);
+  // play a3
+  tone(10, 220, 900);
+  delay(1000);
+  // play b3
+  tone(10,246.94, 300);
+  delay(400);
+   // play c4
+  tone(10, 261.63, 300);
+  delay(400);
+  // play d4
+  tone(10, 293.66, 300);
+  delay(400);
+  // play e4
+  tone(10, 329.63, 900);
+  delay(1000);
+  // play g3
+  tone(10, 196, 300);
+  delay(400);
+  // play f4
+  tone(10, 349.23, 300);
+  delay(400);
+  //play e4
+  tone(10, 329.23, 300);
+  delay(400);
+  // play d4
+  tone(10, 293.63, 900);
+  delay(1000);
+  // play e3
+  tone(10,164.81, 300);
+  delay(400);
+  // play e4
+  tone(10, 329.63, 300);
+  delay(400);
+  // play d4
+  tone(10, 293.63, 300);
+  delay(400);
+  // play c4
+  tone(10, 261.63, 900);
+  delay(1000);
+  // play d3
+  tone(10, 146.83, 300);
+  delay(400);
+    // play d4
+  tone(10, 293.63, 300);
+  delay(400);
+  // play c4
+  tone(10, 261.63, 300);
+  delay(400);
+  // play b3
+  tone(10, 246.94, 900);
+  delay(1000);
+  delay(400);
+  // play e4
+  tone(10, 329.63, 300);
+  delay(400);
+  // play d4# 
+  tone(10, 311.13, 300);
+  delay(350);
+  // play e4
+  tone( 10, 329.63, 300);
+  delay(350);
+  // play d4# 
+  tone( 10,311.13, 300);
+  delay(350);
+  // play e4
+  tone(10, 329.63, 300);
+  delay(350);
+  // play b3
+  tone( 10, 246.94, 300);
+  delay(400);
+  // play d4
+  tone(10, 293.66,300);
+  delay(400);
+  // play c4
+  tone(10, 261.63,300);
+  delay(400);
+  // play a3
+  tone(10, 220, 900);
+  delay(1000);
+  // play d3
+  tone(10,146.83, 300);
+  delay(350);
+  //play f3
+  tone(10, 174.61, 300);
+  delay(400);
+  //play a3
+  tone(10, 220, 300);
+  delay(400);
+  // play b3
+  tone(10, 246.94, 900);
+  delay(1000);
+  // play f3
+  tone(10, 174.61, 300);
+  delay(400);
+  // play a3
+  tone(10, 233.08, 300);
+  delay(400);
+  // play b3
+  tone(10, 246.94, 300);
+  delay(400);
+  // play c4
+  tone(10, 261.63, 900);
+  delay(1000);
+  delay(300);
+  // play e4
+  tone(10, 329.63, 300);
+  delay(400);
+  // play d4#
+  tone(10, 311.13, 300);
+  delay(400);
+  // play e4
+  tone(10, 329.63, 300);
+  delay(400);
+  // play d4#
+  tone(10, 311.13, 300);
+  delay(400);
+  // play e4
+  tone(10, 329.63, 300);
+  delay(400);
+  // play b3
+  tone(10, 246.94, 300);
+  delay(400);
+  // play d4
+  tone(10, 293.66, 300);
+  delay(400);
+  // play c4
+  tone(10, 261.63, 300);
+  delay(400);
+  // play a3
+  tone(10, 220, 900);
+  delay(1000);
+  // play d3
+  tone(10, 146.83, 300);
+  delay(400);
+  // play f3
+  tone(10, 174.61, 300);
+  delay(400);
+  // play a3
+  tone(10, 220, 300);
+  delay(400);
+  // play b3
+  tone(10, 246.94, 900);
+  delay(1000);
+  // play f3
+  tone(10, 174.61, 300);
+  delay(400);
+  // play c4
+  tone(10, 261.63, 300);
+  delay(400);
+  // play b3
+  tone(10, 246.94, 300);
+  delay(400);
+  // play a3
+  tone(10, 220, 900);
+  delay(1000);
+  
+  delay(5000);
+
+
+}
+
+ 
